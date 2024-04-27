53b89ed2989585c67dd3e0ff857f9e0c05408341
Your Name <you@example.com>
2024-04-28 00:36:22 +0300

    sync

--- main.c
+++ main.c
@@ -2,69 +2,103 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "list.h"
-#include "exit_stat.h"
 
 void printStrList(List *list);
+void readBufferLinesIntoList(List *list, char *buf, int bufLen);
+void readFileIntoBuffer(char *name, char **target, int *size);
+
+enum MarkdownElementTypes {
+  HEADER1 = 1,
+  HEADER2,
+  HEADER3,
+  HEADER4,
+  HEADER5,
+  HEADER6,
+  BLOCK_QOUTE,
+  LIST,
+  ULIST,
+  CODE_BLOCK,
+  RULER,
+  IMAGE
+};
+// for more elements:
+// https://www.markdownguide.org/cheat-sheet/
 
 int main(int argc, char **argv) {
   if (argc > 2 || argc < 2) {
     fprintf(stderr, "Too many or too few arguments.\n");
-    return EXIT_INV_ARG;
-  }
-  FILE *file = fopen(argv[1], "r");
-  if (file == NULL) {
-    fprintf(stderr, "Could not open file %s", argv[1]);
-    return EXIT_FILE_ERR;
-  }
-  fseek(file, 0, SEEK_END);
-  int fileSize = ftell(file);
-  fseek(file, 0, SEEK_SET);
-  char *fileBuffer = malloc(sizeof(char) * fileSize + 1);
-  if (fileBuffer == NULL) {
-    perror("Could not allocate space for file buffer.");
-    fclose(file);
-    return EXIT_MAL_FAIL;
+    return EXIT_FAILURE;
   }
 
-  fread(fileBuffer, 1, fileSize, file);
-  fileBuffer[fileSize] = '\0';
-  fclose(file);
+  char *fileBuffer = NULL;
+  int fileSize = 0;
+  readFileIntoBuffer(argv[1], &fileBuffer, &fileSize);
 
   List *lines = CreateList(20);
-  printf("%i, %i\n", lines->size, lines->capcity);
-  int startIndex = 0;
-  int endIndex = 0;
-  for (int i=0; i<fileSize; ++i) {
-    if (fileBuffer[i] == '\n') {
-      endIndex = i;
-      int difference = endIndex - startIndex + 1;
-      char *line = calloc(difference, sizeof(char));
-      if (line == NULL) {
-        perror("Could not allocate space for line.");
-        FreeList(lines);
-        free(fileBuffer);
-        return EXIT_MAL_FAIL;
-      }
-      for (int j=startIndex; j<endIndex; ++j) {
-        char tmp[2] = {fileBuffer[j], '\0'};
-        strcat(line, tmp);
-      }
-      startIndex = endIndex + 1;
-      PushList(lines, line);
-    }
-  }
+  readBufferLinesIntoList(lines, fileBuffer, fileSize);
   free(fileBuffer);
 
-  printf("%i, %i\n", lines->size, lines->capcity);
-
-  //printStrList(lines);
+  printStrList(lines);
 
   FreeList(lines);
   return 0;
 }
 
+void readFileIntoBuffer(char *name, char **target, int *size) {
+  if (*target != NULL) {
+    perror("There maybe a memory leak because target is not NULL in readFileIntoBuffer()");
+  }
+
+  FILE *file = fopen(name, "r");
+  if (file == NULL) {
+    fprintf(stderr, "Could not open file: %s", name);
+    abort();
+  }
+
+  fseek(file, 0, SEEK_END);
+  int fileSize = ftell(file);
+  fseek(file, 0, SEEK_SET);
+
+  *target = (char *)malloc(sizeof(char) * fileSize + 1);
+  if (target == NULL) {
+    perror("Could not allocate space for file buffer.");
+    abort();
+  }
+  fread(*target , 1, fileSize, file);
+  (*target)[fileSize] = '\0';
+  fclose(file);
+
+  *size = fileSize;
+}
+
+void readBufferLinesIntoList(List *list, char *buf, int bufLen) {
+  if (buf == NULL) {
+    perror("Given buffer in readFileIntoBuffer() is NULL.");
+    abort();
+  }
+  int startIndex = 0;
+  int endIndex = 0;
+  for (int i=0; i<bufLen; ++i) {
+    if (buf[i] == '\n') {
+      endIndex = i;
+      int difference = endIndex - startIndex + 1;
+      char *line = calloc(difference, sizeof(char));
+      if (line == NULL) {
+        perror("Could not allocate space for line in readBufferLinesIntoList().");
+        abort();
+      }
+      for (int j=startIndex; j<endIndex; ++j) {
+        char tmp[2] = {buf[j], '\0'};
+        strcat(line, tmp);
+      }
+      startIndex = endIndex + 1;
+      PushList(list, line);
+    }
+  }
+}
+
 void printStrList(List *list) {
   for (int i=0; i<list->size; ++i) {
-    printf("Line: %s\n", (char *)list->elements[i]);
+    printf("Element: %s\n", (char *)list->elements[i]);
   }
 }
